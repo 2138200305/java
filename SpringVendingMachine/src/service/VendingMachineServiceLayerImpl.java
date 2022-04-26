d437dc527b2aa79970b846d40eb088114e85a658
Jae Nwawe <nwawe.jae@gmail.com>
2022-04-26 16:00:45 -0500

    runs with POM.xml

---
+++ SpringVendingMachine/src/service/VendingMachineServiceLayerImpl.java
@@ -0,0 +1,54 @@
+package service;
+
+import dao.VendingMachineAuditDao;
+import dao.VendingMachineDao;
+import dao.VendingMachineDaoFileImpl;
+import dao.VendingMachinePersistenceException;
+import dto.VendingMachineItem;
+
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Map;
+public class VendingMachineServiceLayerImpl implements VendingMachineServiceLayer  {
+    VendingMachineDao dao;
+    VendingMachineAuditDao auditDao;
+    Coins coins;
+
+    public VendingMachineServiceLayerImpl(VendingMachineDao dao, VendingMachineAuditDao auditDao, Coins coins) {
+        this.dao = dao;
+        this.auditDao = auditDao;
+        this.coins=coins;
+    }
+
+    @Override
+    public List<VendingMachineItem> getAvailableItems() throws VendingMachinePersistenceException {
+        return dao.getVendingMachineInventory();
+    }
+
+    @Override
+    public BigDecimal buyItem(String name, BigDecimal inputCash) throws VendingMachinePersistenceException, InsufficientFundsException, NoItemInventoryException {
+        if(dao.getVendingMachineItem(name)==null){
+            throw new VendingMachinePersistenceException("Item not found");
+        }else if(dao.getVendingMachineItem(name).getAmount()==0){
+            throw new NoItemInventoryException("There is no "+ name+" inventory left");
+        }
+        else if(dao.getVendingMachineItem(name).getCost().compareTo(inputCash) > 0) {
+            throw new InsufficientFundsException("You didn't enough insert enough");
+        }
+        VendingMachineItem boughtItem = dao.getVendingMachineItem(name);
+        boughtItem.setAmount(boughtItem.getAmount() -1);
+        dao.updateVendingMachineItem(name,boughtItem);
+        BigDecimal outputCash = inputCash.subtract(boughtItem.getCost()).setScale(2, RoundingMode.HALF_UP);
+        auditDao.writeAuditEntry("1 item sold. Stock updated for " +boughtItem.getName());
+        return outputCash;
+    }
+
+    @Override
+    public int[] giveChange(BigDecimal outputCash) {
+        int outputCashInPennies = outputCash.multiply(BigDecimal.valueOf(100)).intValueExact();
+        return coins.calculateCoins(outputCashInPennies);
+    }
+}
+
