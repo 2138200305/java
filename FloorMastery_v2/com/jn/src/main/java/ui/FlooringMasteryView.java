bf2f79cca42da9237f5d5767f53fba5bfe1f6f81
Jae Nwawe <nwawe.jae@gmail.com>
2022-05-01 13:58:57 -0500

    first

---
+++ FloorMastery_v2/com/jn/src/main/java/ui/FlooringMasteryView.java
@@ -0,0 +1,33 @@
+package ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FlooringMasteryView {
+
+    private UserIO io;
+    
+    @Autowired
+    public FlooringMasteryView(UserIO io) {
+        this.io = io;
+    }
+
+    public void displayMenuBanner() {
+        io.print("<<Flooring Program>>");
+    }
+
+    public int printMenuAndGetSelection(){
+        io.print("1. Display Orders");
+        io.print("2. Add an Order");
+        io.print("3. Edit an Order");
+        io.print("4. Remove an Order");
+        io.print("5. Export All Data");
+        io.print("6. Quit");
+
+        return io.readInt("Please select from the list above", 1, 3);
+    }
+
+
+}
